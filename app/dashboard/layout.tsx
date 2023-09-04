343e5b778d2333e8d505e0c427a6c0f6ac8f12cf
JoeMakuta <makutajosue@gmail.com>
2023-09-04 17:19:37 +0200

    Dasgboard Scroll and layout  done

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,10 +7,15 @@ import ProfilMenu from "../../components/dashboard/ProfileMenu";
 import { ConfigProvider } from "antd";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const [responsive, setResponsive] = useState<boolean>(true);
   return (
     <ConfigProvider theme={{ token: { colorPrimary: "#D37E2F" } }}>
-      <section className=" grid grid-cols-[80px_1fr] grid-rows-[80px_1fr]  ">
-        <header className=" flex justify-between p-3 items-center border-b-[1px] border-black/10">
+      <section
+        className={`transition-all  grid grid-cols-[${
+          responsive == true ? 80 : 200
+        }px_1fr] grid-rows-[80px_1fr] relative max-h-screen `}
+      >
+        <header className=" flex  justify-between p-3 items-center border-b-[1px] border-black/10">
           <Logo height={0} width={100} />
           <div className=" flex gap-2 justify-center items-center ">
             <div className=" relative ">
@@ -27,10 +32,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
             <ProfilMenu />
           </div>
         </header>
-        <aside className="border-r-[1px]  border-black/10 flex pt-4 bg-main_color text-white gap-12 flex-col items-center min-h-screen  row-start-1 row-end-3">
-          <DashboardMenu />
+        <aside className="border-r-[1px] max-h-screen relative  border-black/10 flex pt-4 bg-main_color text-white gap-12 flex-col items-center min-h-screen  row-start-1 row-end-3">
+          <DashboardMenu
+            responsive={responsive}
+            setResponsive={setResponsive}
+          />
         </aside>
-        <main className=" p-4 ">{children}</main>
+        <main className=" p-4 overflow-scroll ">{children}</main>
       </section>
     </ConfigProvider>
   );
